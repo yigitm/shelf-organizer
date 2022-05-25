417f0cc5db7a99dc311955d5fd710ac43274bc5b
Yigit Mersin <yigitmersin@gmail.com>
2022-05-26 00:00:15 +0300

    Add video capture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-function App() {
+import { useRef, useEffect } from 'react';
+
+const App = () => {
+  let video = useRef(null);
+  let canvas = useRef(null);
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 1280, heigth: 720 } })
+      .then((capture) => {
+        video.current.srcObject = capture;
+        video.current.play();
+
+        const captureContent = canvas.current.getContext('2d');
+
+        setInterval(() => {
+          canvas.current.width = video.current.videoWidth;
+          canvas.current.height = video.current.videoHeight;
+          captureContent.drawImage(
+            video.current,
+            0,
+            0,
+            video.current.videoWidth,
+            video.current.videoHeight,
+          );
+        }, 100);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <h1 className="text-7xl font-bold underline text-red-400">Hello world!</h1>
+    <>
+      <video ref={video} autoPlay muted hidden />
+      <canvas ref={canvas} />
+    </>
   );
-}
+};
 
 export default App;
